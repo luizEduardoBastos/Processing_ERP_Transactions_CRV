9eeaf30923f6af9ab57b6b3d9939d26a7454c720
Luiz Bastos <luizeduardobastoslima@gmail.com>
2024-04-26 19:57:24 -0300

    Versão beta possuindo novo erro no processamento

--- app.py
+++ app.py
@@ -1,38 +1,64 @@
-import tkinter as tk
+import tkinter as tk, pandas as pd
 from tkinter import filedialog
-import pandas as pd
 
 
-def processar_arquivo():  
-    # Lista Colunas A Remover
-    colunas_indesejadas = ['Tipo', 'Transferência', 'Id da Conta Financeira', 'Nome da Conta Financeira', 'Tipo de Repetiçao', 'Parcela', 'Observação/Descrição', 'Competência', 'Data Conciliação', 'Valor Previsto', 'Valor Realizado', 'Valor Rateio', 'Valor Retenção', 'Quantidade de Parcelas', 'Origem da Renegociação', 'Boleto Gerado', 'Inicial', 'Categoria', 'Departamento', 'Tipo Cliente/Fornecedor', 'Documento Cliente/Fornecedor', 'Forma de Pagamento - Parcela', 'Forma de Pagamento - Quitação', 'Numero da Nota Fiscal', 'Nome Vendedor']
-    # Abrindo Janela para Selecionar Excel:
+def print_error(result_text, message):
+    result_text.insert(tk.END, message)
+
+def process_file(filepath, colunas_indesejadas):  
+    try:
+        if not filepath.lower().endswith(".csv"):
+            return "O arquivo não é um CSV."
+        df = pd.read_csv(filepath) # Loading CSV
+        df = df.astype(str) # All Cells to String
+        # Replace Specified Characters in All Cells
+        df = df.apply(lambda cell: str(cell.replace('ã', 'a').replace('Ã', 'A').replace('õ', 'o').replace('Õ', 'O').replace('ç', 'c').replace('Ç', 'C')))
+        # Remove Unwanted Columns
+        df = df.drop(columns=colunas_indesejadas, errors='ignore') # Remover Colunas Indesejadas
+        df.to_csv("movimentacoes-filtradas.csv", index=False) # Save/Name File
+        return df.head()
+    except Exception as e:
+            return e
+        
+def process_and_display(result_text, colunas_indesejadas):
+    # Open Select File Window
     filepath = filedialog.askopenfilename(filetypes=[("Arquivos CSV", "*.csv")])
-    # Verificando se 'Arquivo foi Selecionado' == True:
-    if filepath:
-        try:
-            # Carregar Arquivo CSV
-            df = pd.read_csv(filepath)
-            # Remover Colunas Indesejadas
-            df = df.drop(columns=colunas_indesejadas, errors='ignore')
-            # Salvar Novo Arquivo CSV
-            edited_csv_filepath = filepath.rsplit('.', 1)[0] + 'movimentações-filtradas.csv'  # Nome do novo arquivo CSV editado
-            df.to_csv(edited_csv_filepath, index=False)
-            # Exibindo 1ªs linhas do DF:
-            result_text.insert(tk.END, df.head())
-        except Exception as e:
-            result_text.insert(tk.END, f"Erro ao processar arquivo: {e}")
+    if filepath: # If Selected:
+        result = process_file(filepath, colunas_indesejadas)
+        if isinstance(result, str):  # If Result == Error Message
+            return print_error(result_text, result)
+        else:  
+            return result_text.insert(tk.END, result)
 
-# Criando Janela Principal:
-root = tk.Tk()
-root.title("Processador (Relatórios Movimentações Bom Controle")
+def gui_setup():
+    # Lista Colunas A Remover
+    colunas_indesejadas = ['Tipo', 'Transferência', 
+    'Id da Conta Financeira', 'Nome da Conta Financeira', 
+    'Tipo de Repetiçao', 'Parcela', 'Observação/Descrição', 
+    'Competência', 'Data Conciliação', 'Valor Previsto', 
+    'Valor Realizado', 'Valor Rateio', 'Valor Retenção', 
+    'Quantidade de Parcelas', 'Origem da Renegociação', 
+    'Boleto Gerado', 'Inicial', 'Categoria', 'Departamento', 
+    'Tipo Cliente/Fornecedor', 'Documento Cliente/Fornecedor', 
+    'Forma de Pagamento - Parcela', 'Forma de Pagamento - Quitação', 
+    'Numero da Nota Fiscal', 'Nome Vendedor']
+    root = tk.Tk() # Main Window:
+    root.title("Filtrar Relatório Movimentação")
 
-# Botão "Processar Arquivo":
-process_button = tk.Button(root, text="Selecionar arquivo e processar", command=processar_arquivo)
-process_button.pack(pady=10)
+    result_text = tk.Text(root, height=10, width=50)
+    result_text.pack()
 
-# Resultado Processamento:
-result_text = tk.Text(root, height=10, width=50)
-result_text.pack()
+    label = tk.Label(root,text="A remover as colunas:")
+    label.pack()
 
-root.mainloop()
+    text = tk.Text(root, height='10', width='80')
+    for i in colunas_indesejadas:
+        text.insert(tk.END, i + '\n')
+    text.pack()
+
+    process_button = tk.Button(root, text="Selecionar arquivo e processar", command=lambda: process_and_display(result_text, colunas_indesejadas))
+    process_button.pack(pady=30, side='top', anchor='center')
+    
+    root.mainloop()
+
+gui_setup()
